c58dbacb5845a006f5a16f59000bff66b315bb8c
Allen Guo <guosonglin001@gmail.com>
2024-05-18 13:53:27 +0800

    dirty

--- t.sh
+++ t.sh
@@ -2,3 +2,7 @@ npm install
 
 npm run compile
 npm run package --no-yarn
+
+# git commit --no-verify -m "disable supportsSystem check"
+# git commit --no-verify -m "disable auto build"
+# micromamba install nodejs=20
